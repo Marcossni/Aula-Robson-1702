7f907f28a1c85f93959864a5a03a40fb1e48b957
Marcossni <marcos.pedromartins02@gmail.com>
2025-02-18 19:33:02 -0300

    Update Program.cs

--- Aula Robson 1702/Program.cs
+++ Aula Robson 1702/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//MARCO
+double x1, x2, y1, y2;
+double d1, d2, d;
+
+Console.WriteLine("Escreva x2");
+x2= int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva x1");
+x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva y2");
+y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva y1");
+y1 = int.Parse(Console.ReadLine());
+
+d1 = Math.Pow(x2 - x1, 2);
+d2 = Math.Pow(y2 - y1, 2);
+d = Math.Sqrt(d1 + d2);
+
+Console.WriteLine(d);
